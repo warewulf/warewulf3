dca36c59191a359f8ac7d53b980262cce06f2c31
jcsiadal <jeremy.c.siadal@intel.com>
2019-06-11 13:35:07 -0700

    Updated VNFS & build script for Ubuntu 18
    Signed-off-by: jcsiadal <jeremy.c.siadal@intel.com>

---
+++ platform/build-Ubuntu-18.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+apt install apache2 libapache2-mod-perl2 tftpd-hpa mysql-server debootstrap isc-dhcp-server tcpdump openssh-client nfs-kernel-server nfs-common rpcbind ntp wget build-essential perl make automake pkg-config busybox e2fsprogs parted libarchive-tools ipxe libtirpc-dev libselinux-dev gcc python python3 libssl-dev uuid-dev libblkid-dev gettext libdevmapper-dev liblzma-dev ipmitool 
+
+if [ $? -eq 0 ]; then
+    for SUBDIR in common cluster vnfs ipmi provision; do
+        cd ../$SUBDIR
+        if [ $? -ne 0 ]; then
+            break
+        fi
+        if [ "$SUBDIR" = "ipmi" ]; then
+          ./autogen.sh --with-local-ipmitool --prefix=/
+        else
+          ./autogen.sh --prefix=/
+        fi
+        if [ $? -eq 0 ]; then
+            make
+        else
+            echo "$SUBDIR: autogen failed"
+            break
+        fi
+        if [ $? -eq 0 ]; then
+            make install
+        else
+            echo "$SUBDIR: make failed"
+            break
+        fi
+        if [ $? -ne 0 ]; then
+            echo "$SUBDIR: make install failed"
+            break
+        fi
+    done
+fi
+

ab97932ad41413a5b3fe383f6ec422d2767a4088
jcsiadal <jeremy.c.siadal@intel.com>
2019-06-17 12:08:42 -0700

    Update example build script
    Signed-off-by: jcsiadal <jeremy.c.siadal@intel.com>

--- platform/build-Ubuntu-18.sh
+++ platform/build-Ubuntu-18.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install apache2 libapache2-mod-perl2 tftpd-hpa mysql-server debootstrap isc-dhcp-server tcpdump openssh-client nfs-kernel-server nfs-common rpcbind ntp wget build-essential perl make automake pkg-config libarchive-tools ipxe libtirpc-dev libselinux-dev gcc python python3 libssl-dev uuid-dev libblkid-dev gettext libdevmapper-dev liblzma-dev ipmitool 
+apt install apache2 libapache2-mod-perl2 tftpd-hpa mysql-server debootstrap isc-dhcp-server tcpdump openssh-client nfs-kernel-server nfs-common rpcbind ntp wget build-essential perl make automake pkg-config libarchive-tools ipxe libtirpc-dev libselinux-dev gcc python python3 libssl-dev uuid-dev libblkid-dev gettext libdevmapper-dev liblzma-dev ipmitool libdbi-perl libdbd-mysql-perl
 
 if [ $? -eq 0 ]; then
     for SUBDIR in common cluster vnfs ipmi provision; do
@@ -14,15 +14,9 @@ if [ $? -eq 0 ]; then
         fi
           ./autogen.sh --prefix=/ --bindir=/usr/bin $OPTIONS
         if [ $? -eq 0 ]; then
-            make
-        else
-            echo "$SUBDIR: autogen failed"
-            break
-        fi
-        if [ $? -eq 0 ]; then
             make install
         else
-            echo "$SUBDIR: make failed"
+            echo "$SUBDIR: autogen failed"
             break
         fi
         if [ $? -ne 0 ]; then
